67552094ca86b14978f60eec2709cdb4ac45e307
GabCB <gabycuencas@gmail.com>
2023-01-29 00:52:30 +0100

    url and fs modules

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const http = require('http');
+    fs = require('fs'),
+    url = require('url');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
-}).listen(8080);
+  let addr = request.url,
+    q = url.parse(addr, true),
+    filePath = '';
 
+    if (q.pathname.includes('documentation')) {
+        filePath = (__dirname + '/documentation.html');
+    } else {
+        filePath = 'index.html'
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+    
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+    });
+    
+}).listen(8080);
 console.log('My first Node test server is running on Port 8080.');
