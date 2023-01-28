10d76708989d5af9280bf0f29bc2a78d040ed98f
GabCB <gabycuencas@gmail.com>
2023-01-29 00:59:31 +0100

    added appendFile method  (fs module)

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ http.createServer((request, response) => {
     q = url.parse(addr, true),
     filePath = '';
 
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+        if (err) {
+      console.log(err);
+        } else {
+            console.log('Added to log.');
+        }
+    });
+
     if (q.pathname.includes('documentation')) {
         filePath = (__dirname + '/documentation.html');
     } else {
