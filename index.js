541fed9cfec970b8a5917bc33ac5d12bcec3531c
GabCB <gabycuencas@gmail.com>
2023-02-21 17:15:00 +0100

    update allowedOrigins

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 const cors = require("cors");
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+let allowedOrigins = ["http://localhost:8080", "https://moviewebapp.herokuapp.com/"];
 
 app.use(cors({
     origin: (origin, callback) => {
