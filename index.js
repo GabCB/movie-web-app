7b4c54af581d31f98bac3b826d589272d347fc9d
GabCB <gabycuencas@gmail.com>
2023-03-22 14:56:57 +0100

    update syntax to enable CORS

--- index.js
+++ index.js
@@ -32,14 +32,7 @@ const cors = require("cors");
 let allowedOrigins = ["http://localhost:8080", "http://localhost:1234", "https://moviewebapp.herokuapp.com/"];
 
 app.use(cors({
-    origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){//If a specific origin isn't found on the list of allowed origins
-        let message ="The CORS policy for this application doesn't allow access from origin " + origin;
-        return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-    }
+    origin: '*'
 }));
 
 let auth = require("./auth")(app);
